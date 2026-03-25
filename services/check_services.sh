1a4bf7ae37b308a11a76dc6e04ea61ff9a0438da
manoel cândido <candidomanoel78@gmail.com>
2026-03-25 13:42:56 -0300

    refactor: passa a lógica de verificação para uma função reutilizável

--- services/check_services.sh
+++ services/check_services.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-SERVICE1="sleep"
-SERVICE2="tomcat"
+check_services() {
+    local SERVICE=$1
 
-if pgrep -x "$SERVICE1" > /dev/null; then
-    echo "$SERVICE1 is ON"
-    echo "$SERVICE1 has been active for $(ps -p $(pgrep -x "$SERVICE1") -o etime=)"
-else
-    echo "$SERVICE1 is OFF"
-fi
+    if pgrep -x "$SERVICE" > /dev/null; then
+        echo "$SERVICE is ON"
+        echo "$SERVICE has been active for $(ps -p $(pgrep -x "$SERVICE") -o etime=)"
+    else 
+        echo "$SERVICE in OFF"
+    fi
+}   
 
-if pgrep -x "$SERVICE2" > /dev/null; then
-    echo "$SERVICE2 is ON"
-    echo "$SERVICE2 has been active for $(ps -p $(pgrep -x "$SERVICE2") -o etime=)"
-else
-    echo "$SERVICE2 is OFF"
-fi
+check_services "jboss"
+check_services "tomcat"
